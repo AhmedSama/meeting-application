8c55711748bb05cf8f3240fd990f1080c407d741
AhmedSama <nwnwlala@yahoo.com>
2022-06-17 11:47:39 +0300

    adding my linkden page

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -41,6 +41,8 @@ export const Create = () => {
             </button>
           </div>
           <p className="small"><Link className="link" to={"/join"}>join</Link> a meet</p>
+          <p className="small"> Developed by <a href="https://www.linkedin.com/in/ahmed-ghazi-samari
+" >Ahmed Ghazi</a> with ❤</p>
         </div>
   )
 }
