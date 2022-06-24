4adc83be0865a1b73ebf498a8cc4636202da595f
AhmedSama <nwnwlala@yahoo.com>
2022-06-24 14:50:19 +0300

    adding new icon

--- client/src/pages/EndCall.js
+++ client/src/pages/EndCall.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import { MdCallEnd } from 'react-icons/md'
 import { Link } from 'react-router-dom'
-import endcall from '../images/meet.svg'
+// import endcall from '../images/endcall.svg'
+import icon from '../images/meet.svg'
+
 export const EndCall = () => {
   return (
     <div className='end-call'>
         <div className='image'>
             <div className='img-container'>
-                <img src={endcall} alt="end call" />
+                <img src={icon} alt="end call" />
             </div>
         </div>
         <div className='contact'>
         <h2 className="title flex items-center">
             call ended
         <div  className=' flex-center  m-10'>
-              <MdCallEnd className='icon color-danger'/>
+              <MdCallEnd className='icon'/>
             </div>
         </h2>   
-        <h2 className="small"> Contact me <a rel="noreferrer" href="https://www.linkedin.com/in/ahmed-ghazi-samari
-" target="_blank">Ahmed Ghazi</a> ❤ </h2>
         <div className='btns'>
             <Link to={"/"} className='btn' >create a new meet</Link>
             <Link to={"/join"} className='btn'>join a meet</Link>
         </div>
+        <h2 className="small"> Contact me <a rel="noreferrer" href="https://www.linkedin.com/in/ahmed-ghazi-samari
+" target="_blank">Ahmed Ghazi</a> ❤ </h2>
         </div>
     </div>
   )
